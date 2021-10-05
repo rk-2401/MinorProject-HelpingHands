eab24bac3ca277d5ba3f5b427dc1772e57777849
Rachit <rachitk115@gmail.com>
2021-10-05 20:00:06 +0530

    carousel

--- app.js
+++ app.js
@@ -5,37 +5,84 @@ class Header extends React.Component{
     render(){
         return(
             <div id="header">
-                <div id="div-logo-name-navbar">
+                <div id="div-logo-name-header">
+                    <div id="menu-btn">
+                        <hr className="menu-btn-line"></hr>
+                        <hr className="menu-btn-line"></hr>
+                        <hr className="menu-btn-line"></hr>
+                    </div>
                     <div id="div-logo-name">
                         <div id="div-company-logo"><img id="company-logo" src="/images/Logo.jpg" alt="Company Logo"/></div>
-                        <div id="div-company-name"><p id="company-name">HELPING HANDS</p></div>
+                        <div id="div-company-name"><p id="company-name">Helping Hands</p></div>
                     </div>
-                    <NavBar/>
+                    <User/>
                 </div>
-                <p id="tagline">Bringing Happiness With Maintenance</p>
             </div>
         );
     };
 }
 
 
-class NavBar extends React.Component{
+class User extends React.Component{
     constructor(props){
         super(props);
         this.state={
-            click:false
+            login:false,
+            username:'Rachit'
         }
     }
     render(){
-        if(this.state.click==false)
+        if(this.state.login==true)
         return(
-            <div id="nav-bar">
-               <hr class="nav-line"></hr>
-               <hr class="nav-line"></hr>
-               <hr class="nav-line"></hr>
+            <div id="user">
+                <div id="username">
+                    <i id="user-icon" className="fa fa-user" aria-hidden="true"></i>
+                    {this.state.username}
+                </div>
+            </div>
+        );
+        else
+        return(
+            <div id="user">
+                <button type="button" className="login-btn">Login / Sign Up</button>
             </div>
         );
     };
 }
 
-ReactDOM.render(<Header/>,document.getElementById('root'));
+class Carousel extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            
+        }
+    }
+    render(){
+        return(
+            <div id="carousel">
+                <div><i id="carousel-arrow-l" className="fas fa-angle-left carousel-arrow"></i></div>
+                <div><i id="carousel-arrow-r" className="fas fa-angle-right carousel-arrow"></i></div>
+            </div>
+        );
+    };
+}
+
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            
+        }
+    }
+    render(){
+        return(
+            <div>
+                <Header/>
+                <div id="div-tagline"><p id="tagline">Bringing Happiness With Maintenance</p></div>
+                <Carousel/>
+            </div>
+        );
+    };
+}
+
+ReactDOM.render(<App/>,document.getElementById('root'));
