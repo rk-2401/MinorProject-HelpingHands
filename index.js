a27f84d8d6b5d3822e7a7d4302394178c9a7f606
MD SAJID ANIS <sajid.anis20@gmail.com>
2021-12-30 17:38:30 +0530

    Revert "Backend models"

--- index.js
+++
@@ -1,8 +0,0 @@
-import { start } from "./src/backend/server";
-const path = require('path')
-require('dotenv').config({
-    path: path.resolve(__dirname, '.env'),
-});
-import { User } from './src/backend/Models/user/user-model';
-
-start();
